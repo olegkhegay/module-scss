f35156464493176e8e56c546a2b0ed30ac7d9a59
Oleg <oxegay@gmail.com>
2024-11-25 17:06:15 +0500

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import s from './Header.module.scss'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+        <header className={s.header}>
+            <div className="container">
+                <nav className={s.nav}>
+                    <Link className={s.logo}>SHOP.CO</Link>
+
+                    <div className={s.menu}>
+                        <Link>Shop</Link>
+                        <Link>On Sale</Link>
+                        <Link>New Arrivals</Link>
+                        <Link>Brands</Link>
+                    </div>
+
+                    <input type="text" className={s.search}  placeholder='Search for products...'/>
+
+                    <div className={s.box}>
+                        <img src="/icon-1.svg" alt="" />
+                        <img src="/icon-2.svg" alt="" />
+                    </div>
+
+                    
+                </nav>
+            </div>
+        </header>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.module.scss'
+
+const Hero = () => {
+  return (
+    <>
+        <div className="wrap">
+            <div className="box">
+                <h1>HERO</h1>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Hero
