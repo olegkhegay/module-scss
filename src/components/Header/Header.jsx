0ee7a58dd07f38de36c404df21ef04ffc38a43ab
Oleg <oxegay@gmail.com>
2024-12-16 17:01:17 +0500

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,56 @@
-import React from 'react'
-import s from './Header.module.scss'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import s from './Header.module.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Длительность анимации (в миллисекундах)
+      offset: 50,     // Смещение (в пикселях) для начала анимации
+      easing: 'ease-in-out', // Плавность анимации
+      once: true,     // Анимация срабатывает только один раз
+    });
+  }, []);
+
   return (
     <>
-        <header className={s.header}>
-            <div className="container">
-                <nav className={s.nav}>
-                    <Link to={'/'} className={s.logo}>SHOP.CO</Link>
+      <header className={s.header} data-aos="fade-down">
+        <div className="container">
+          <nav className={s.nav}>
+            {/* Логотип */}
+            <Link to={'/'} className={s.logo} data-aos="fade-down" data-aos-delay="100">
+              SHOP.CO
+            </Link>
 
-                    <div className={s.menu}>
-                        <Link>Shop</Link>
-                        <Link>On Sale</Link>
-                        <Link>New Arrivals</Link>
-                        <Link>Brands</Link>
-                    </div>
-
-                    <input type="text" className={s.search}  placeholder='Search for products...'/>
-
-                    <div className={s.box}>
-                        <img src="/icon-1.svg" alt="" />
-                        <img src="/icon-2.svg" alt="" />
-                    </div>
-
-                    
-                </nav>
+            {/* Меню */}
+            <div className={s.menu}>
+              <Link data-aos="fade-down" data-aos-delay="200">Shop</Link>
+              <Link data-aos="fade-down" data-aos-delay="300">On Sale</Link>
+              <Link data-aos="fade-down" data-aos-delay="400">New Arrivals</Link>
+              <Link data-aos="fade-down" data-aos-delay="500">Brands</Link>
             </div>
-        </header>
-    </>
-  )
-}
 
-export default Header
+            {/* Поле поиска */}
+            <input
+              type="text"
+              className={s.search}
+              placeholder="Search for products..."
+              data-aos="fade-down"
+              data-aos-delay="600"
+            />
+
+            {/* Иконки */}
+            <div className={s.box}>
+              <img src="/icon-1.svg" alt="Icon 1" data-aos="fade-down" data-aos-delay="700" />
+              <img src="/icon-2.svg" alt="Icon 2" data-aos="fade-down" data-aos-delay="800" />
+            </div>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
